c0c70dd67868b277f35132795a268650677b78de
eliasculqui03 <7565596321@untrm.edu.pe>
2025-01-31 12:01:32 -0500

    Interfaz simple

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    'node_modules/preline/dist/*.js',
+],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
+
